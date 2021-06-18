9eba04432763062fc703d08930d63b108192ea43
Ryan Dens <ryandens735@gmail.com>
2021-06-18 16:23:31 -0400

    :sparkles: prepare for publishing

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,12 +1,19 @@
 plugins {
     `java-gradle-plugin`
     `maven-publish`
+    id("com.gradle.plugin-publish") version "0.14.0"
     id("org.jetbrains.kotlin.jvm") version "1.4.31"
 }
 
 repositories {
     mavenCentral()
     gradlePluginPortal()
+}
+
+pluginBundle {
+    website = "https://www.ryandens.com"
+    vcsUrl = "https://github.com/ryandens/javaagent-gradle-plugin"
+    tags = listOf("javaagent", "instrumentation", "docker", "jib", "application")
 }
 
 dependencies {
@@ -34,18 +41,26 @@ gradlePlugin {
     plugins {
         create("javaagentApplicationPlugin") {
             id = "com.ryandens.javaagent-application"
+            displayName = "Javaagent Application Plugin"
+            description = "Automatically attaches javaagents to the Application Plugin run tasks and the main application distributions"
             implementationClass = "com.ryandens.javaagent.JavaagentApplicationPlugin"
         }
         create("javaagentApplicationDistributionPlugin") {
             id = "com.ryandens.javaagent-application-distribution"
+            displayName = "Javaagent Application Distribution Plugin"
+            description = "Automatically attaches javaagents to the Application Plugin distributions"
             implementationClass = "com.ryandens.javaagent.JavaagentApplicationDistributionPlugin"
         }
         create("javaagentApplicationRunPlugin") {
             id = "com.ryandens.javaagent-application-run"
+            displayName = "Javaagent Application Run Plugin"
+            description = "Automatically attaches javaagents to the Application Plugin run tasks"
             implementationClass = "com.ryandens.javaagent.JavaagentApplicationRunPlugin"
         }
         create("javaagentJibPlugin") {
             id = "com.ryandens.javaagent-jib"
+            displayName = "Javaagent Jib Plugin"
+            description = "Automatically includes javaagents in OCI images created by Jib"
             implementationClass = "com.ryandens.javaagent.JavaagentJibExtension"
         }
     }

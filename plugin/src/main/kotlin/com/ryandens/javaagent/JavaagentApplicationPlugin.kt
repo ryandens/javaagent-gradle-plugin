b127f98302aa286c7290d1a93246500ae3bfb3f0
Ryan Dens <ryandens735@gmail.com>
2021-06-18 11:34:15 -0400

    :bulb: add javadoc

--- plugin/src/main/kotlin/com/ryandens/javaagent/JavaagentApplicationPlugin.kt
+++ plugin/src/main/kotlin/com/ryandens/javaagent/JavaagentApplicationPlugin.kt
@@ -3,6 +3,10 @@ package com.ryandens.javaagent
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
+/**
+ * Convenience plugin to apply for those who want to take advantage of both the [JavaagentApplicationRunPlugin] and the
+ * [JavaagentApplicationDistributionPlugin].
+ */
 class JavaagentApplicationPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         target.pluginManager.apply(JavaagentApplicationDistributionPlugin::class.java)

062dd4ec0131db83efae1aac3caf66796c86d47f
Ryan Dens <ryandens735@gmail.com>
2021-06-18 15:42:41 -0400

    :recycle: target 1.8

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -19,6 +19,17 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
 
+tasks {
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
 gradlePlugin {
     plugins {
         create("javaagentApplicationPlugin") {

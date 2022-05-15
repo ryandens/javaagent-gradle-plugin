36cc0cf459c57edc37ccd48d93f873589c1e8f05
Ryan Dens <ryandens735@gmail.com>
2022-05-14 22:38:22 -0700

    :arrow_up: upgrade release version

--- example-projects/buildSrc/build.gradle.kts
+++ example-projects/buildSrc/build.gradle.kts
@@ -8,6 +8,6 @@ repositories {
 }
 
 dependencies {
-  implementation("com.ryandens:otel:0.2.2")
-  implementation("com.ryandens:plugin:0.2.2")
+  implementation("com.ryandens:otel:0.3.0")
+  implementation("com.ryandens:plugin:0.3.0")
 }

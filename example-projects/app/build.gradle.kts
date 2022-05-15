614968d2d46662567a9004bafb734858619adcd7
Ryan Dens <ryandens735@gmail.com>
2022-05-14 18:32:16 -0700

    :sparkles: enhance example app to consume custom instrumentation and leverage javaagent-application plugin to run said extended agent

--- example-projects/app/build.gradle.kts
+++ example-projects/app/build.gradle.kts
@@ -1,11 +1,14 @@
 plugins {
     id("com.ryandens.javaaagent.example.java-application-conventions")
     id("com.ryandens.javaagent-otel")
+    id("com.ryandens.javaagent-application")
 }
 
 dependencies {
   otel("io.opentelemetry.javaagent:opentelemetry-javaagent:1.12.0")
+  javaagent(files("$buildDir/agents/extended-opentelemetry-javaagent.jar"))
   otelExtension("io.opentelemetry.contrib:opentelemetry-samplers:1.12.0-alpha")
+  otelInstrumentation(project(":custom-instrumentation"))
 }
 
 application {

c55694f654dcb552535c322a7df271facb05bd05
Ryan Dens <ryandens735@gmail.com>
2022-05-14 17:36:16 -0700

    :sparkles: auto register sample instrumentation using SPI

---
+++ example-projects/custom-instrumentation/src/main/java/com/ryandens/javaagent/example/SampleInstrumentationModule.java
@@ -0,0 +1,20 @@
+package com.ryandens.javaagent.example;
+
+import com.google.auto.service.AutoService;
+import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
+import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
+
+import java.util.Collections;
+import java.util.List;
+
+@AutoService(InstrumentationModule.class)
+public final class SampleInstrumentationModule extends InstrumentationModule {
+  public SampleInstrumentationModule() {
+    super("sample");
+  }
+
+  @Override
+  public List<TypeInstrumentation> typeInstrumentations() {
+    return Collections.singletonList(new SampleInstrumentation());
+  }
+}

9e479a1376012840a4b3abde543c1569aaa71f27
Ryan Dens <ryandens735@gmail.com>
2022-11-28 05:44:50 -0800

    :bookmark: increment version for release

--- example-projects/buildSrc/build.gradle.kts
+++ example-projects/buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 }
 /* uncomment when doing local testing on this project, leave commented out for functional test to resolve latest plugin
 dependencies {
-  implementation("com.ryandens:otel:0.4.1")
-  implementation("com.ryandens:plugin:0.4.1")
+  implementation("com.ryandens:otel:0.4.2")
+  implementation("com.ryandens:plugin:0.4.2")
 }
 */

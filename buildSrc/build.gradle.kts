eec560cbd1daa99adf0c1b361ef0ccf448a33093
Ryan Dens <ryandens735@gmail.com>
2022-11-28 05:46:20 -0800

    :bug: Decrease version of kotlin-gradle-plugin to a stable version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,6 +8,6 @@ repositories {
 
 dependencies {
     implementation("com.gradle.publish:plugin-publish-plugin:1.1.0")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0-Beta")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.21")
     implementation("com.netflix.nebula:nebula-publishing-plugin:19.0.0")
 }

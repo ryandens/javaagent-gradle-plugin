bef54705884697377f6897a6063c913a6a4156ac
Punya Biswal <punya.biswal@gmail.com>
2023-07-18 22:39:57 -0400

    Add support for Windows

---
+++ plugin/src/main/kotlin/com/ryandens/javaagent/Platform.kt
@@ -0,0 +1,17 @@
+package com.ryandens.javaagent
+
+import org.gradle.api.internal.plugins.StartScriptTemplateBindingFactory
+import org.gradle.api.internal.plugins.UnixStartScriptGenerator
+import org.gradle.api.internal.plugins.WindowsStartScriptGenerator
+import org.gradle.api.resources.TextResource
+
+enum class Platform(
+    val lineSeparator: String,
+    val pathSeparator: String,
+    val templateBindingFactory: StartScriptTemplateBindingFactory,
+    val template: TextResource,
+) {
+
+    UNIX("\n", "/", StartScriptTemplateBindingFactory.unix(), UnixStartScriptGenerator().template),
+    WINDOWS("\r\n", "\\", StartScriptTemplateBindingFactory.windows(), WindowsStartScriptGenerator().template),
+}

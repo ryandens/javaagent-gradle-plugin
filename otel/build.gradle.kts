7ba1faa3efce0eb72cdc7e28aa288822f5114d3a
Ryan Dens <ryan.dens@pixee.ai>
2023-10-31 09:51:14 -0700

    :rotating_light: fix deprecation warning for not delcaring test suite dependencies

--- otel/build.gradle.kts
+++ otel/build.gradle.kts
@@ -8,6 +8,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.junit.jupiter:junit-jupiter-params")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 gradlePlugin {

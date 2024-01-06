9917a0106da426dbb5b780574eada80fdba40202
Ryan Dens <ryandens735@gmail.com>
2024-01-06 10:30:27 -0800

    :arrow_up: upgrade otel samplers to latest

--- example-projects/app/build.gradle.kts
+++ example-projects/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 dependencies {
   otel("io.opentelemetry.javaagent:opentelemetry-javaagent:1.32.0")
-  otelExtension("io.opentelemetry.contrib:opentelemetry-samplers:1.13.0-alpha")
+  otelExtension("io.opentelemetry.contrib:opentelemetry-samplers:1.32.0-alpha")
   otelInstrumentation(project(":custom-instrumentation", "shadow"))
 }
 

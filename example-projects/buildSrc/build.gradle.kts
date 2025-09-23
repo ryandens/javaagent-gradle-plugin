7bc32904ddd7375b190f78af8d41e06b1f4d3521
Ryan Conway Dens <ryandens735@gmail.com>
2025-09-23 13:31:40 -0400

    :arrow_up: upgrade gradleup and muzzle

--- example-projects/buildSrc/build.gradle.kts
+++ example-projects/buildSrc/build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
     implementation("com.ryandens:otel:0.9.1")
     implementation("com.ryandens:plugin:0.9.1")
     */
-    implementation("io.opentelemetry.instrumentation.muzzle-generation:io.opentelemetry.instrumentation.muzzle-generation.gradle.plugin:2.8.0-alpha")
-    implementation("io.opentelemetry.instrumentation.muzzle-check:io.opentelemetry.instrumentation.muzzle-check.gradle.plugin:2.8.0-alpha")
+    implementation("com.gradleup.shadow:com.gradleup.shadow.gradle.plugin:9.1.0")
+    implementation("io.opentelemetry.instrumentation.muzzle-generation:io.opentelemetry.instrumentation.muzzle-generation.gradle.plugin:2.20.1-alpha")
+    implementation("io.opentelemetry.instrumentation.muzzle-check:io.opentelemetry.instrumentation.muzzle-check.gradle.plugin:2.20.1-alpha")
 }

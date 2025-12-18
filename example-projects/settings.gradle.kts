a96134b269379e1b37142fa518bf01600dda684f
John Y. Pazekha <neboskreb@narod.ru>
2025-12-18 22:36:52 +0100

    Include Android examples into examples build

--- example-projects/settings.gradle.kts
+++ example-projects/settings.gradle.kts
@@ -1,2 +1,24 @@
 rootProject.name = "example-projects"
-include("app", "custom-instrumentation")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+    includeBuild("..")
+
+    plugins {
+        id("com.android.application").version("8.13.2")
+        id("com.android.library").version("8.13.2")
+        id("de.mannodermaus.android-junit5").version("1.14.0.0")
+        id("com.ryandens.plugin-conventions")
+        id("com.ryandens.javaagent-test-android")
+        id("org.gradle.toolchains.foojay-resolver-convention").version("1.0.0")
+    }
+}
+
+includeBuild("..")
+
+include("app", "custom-instrumentation", "android-app", "android-library")

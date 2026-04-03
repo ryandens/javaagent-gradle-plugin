7cd07eae96ead4b9f7feb55f9b3d36aa8bb52d83
Ryan Dens <ryandens735@gmail.com>
2026-04-03 15:32:05 -0700

    🔖 tag 0.11.0 (#337)

--- example-projects/buildSrc/build.gradle.kts
+++ example-projects/buildSrc/build.gradle.kts
@@ -8,8 +8,8 @@ repositories {
 }
 dependencies {
     /* uncomment when doing local testing on this project, leave commented out for functional test to resolve latest plugin
-    implementation("com.ryandens:otel:0.9.1")
-    implementation("com.ryandens:plugin:0.9.1")
+    implementation("com.ryandens:otel:0.11.0")
+    implementation("com.ryandens:plugin:0.11.0")
     */
     implementation("com.gradleup.shadow:com.gradleup.shadow.gradle.plugin:9.4.1")
     implementation("io.opentelemetry.instrumentation.muzzle-generation:io.opentelemetry.instrumentation.muzzle-generation.gradle.plugin:2.20.1-alpha")

a9897bb98eee9d1865c5758dd45b334e80ea1e09
Ryan Dens <ryandens735@gmail.com>
2026-04-26 17:43:09 -0700

    :wrench: publish common plugin

--- jib-common/build.gradle.kts
+++ jib-common/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.plugin.compatibility.compatibility
+
 plugins {
     id("com.ryandens.plugin-conventions")
 }
@@ -6,4 +8,21 @@ dependencies {
     implementation(project(":plugin"))
     compileOnly("com.google.cloud.tools:jib-gradle-plugin-extension-api:0.4.0")
     compileOnly("com.google.cloud.tools.jib:com.google.cloud.tools.jib.gradle.plugin:3.5.3")
+}
+
+gradlePlugin {
+    plugins {
+        create("javaagentJibCommonPlugin") {
+            id = "com.ryandens.javaagent-jib-common"
+            displayName = "Javaagent Jib Common Plugin"
+            description = "Come plugin to help automatically attach javaagents in OCI images created by jib-like plugins"
+            implementationClass = "com.ryandens.javaagent.jib.JavaagentJibExtension"
+            tags.set(listOf("javaagent", "instrumentation", "docker", "jib"))
+            compatibility {
+                features {
+                    configurationCache = true
+                }
+            }
+        }
+    }
 }
